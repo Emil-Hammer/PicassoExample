c0042e1d2b0dff7fc8ce15fd0bb787e8e5b2ee62
Emil Hammer <mr_h4mm3r@live.dk>
2019-12-14 21:12:35 +0100

    Tilføjet Picasso og et billed.

--- app/src/main/java/me/hammer/picassoexample/MainActivity.java
+++ app/src/main/java/me/hammer/picassoexample/MainActivity.java
@@ -1,7 +1,11 @@
 package me.hammer.picassoexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        Picasso.get().load("http://i.imgur.com/7UwaS48.jpg").resize(150, 150).into(imageView);
     }
 }
